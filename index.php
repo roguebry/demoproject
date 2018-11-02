59458acd6b0e5768fccce2e45ee945176973c727
Bryant <44382758+roguebry@users.noreply.github.com>
2018-11-02 17:33:51 -0400

    Test Commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by Bryant N Frankford
+ * nathanielfrankford@gmail.com
+ */
+
+
+?>
+<html>
+<head>
+	<title>Demo Project</title>
+
+<!--Required-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!--Custom-->
+	<script src="js/index.js"></script>
+
+</head>
+<body>
+<div id="maincContainer">
+	<div id="orderContainer"></div>
+	<div id="menuContainer">Click To Start</div>
+</div>
+</body>
+</html>
